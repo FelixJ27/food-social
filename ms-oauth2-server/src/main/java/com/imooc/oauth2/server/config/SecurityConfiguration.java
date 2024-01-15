e719bde17f63eb16108d179b5035c0130cc7b76b
felix <1045985453@qq.com>
2024-01-15 11:08:44 +0800

    security配置类

--- ms-oauth2-server/src/main/java/com/imooc/oauth2/server/config/SecurityConfiguration.java
+++ ms-oauth2-server/src/main/java/com/imooc/oauth2/server/config/SecurityConfiguration.java
@@ -18,65 +18,61 @@ import javax.annotation.Resource;
  */
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    // 注入 Redis 连接工厂
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+    //注入redis连接工厂
     @Resource
     private RedisConnectionFactory redisConnectionFactory;
 
-    // 初始化 RedisTokenStore 用于将 token 存储至 Redis
+    //RedisTokenStore用于将token存储至redis
     @Bean
     public RedisTokenStore redisTokenStore() {
         RedisTokenStore redisTokenStore = new RedisTokenStore(redisConnectionFactory);
-        redisTokenStore.setPrefix("TOKEN:"); // 设置key的层级前缀，方便查询
+        redisTokenStore.setPrefix("TOKEN:");  //设置key的层级前缀，方便查询
         return redisTokenStore;
     }
 
-    // 初始化密码编码器，用 MD5 加密密码
+    //初始化密码编码器，用md5加密密码
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new PasswordEncoder() {
             /**
              * 加密
-             * @param rawPassword 原始密码
+             * @param charSequence 原始密码
              * @return
              */
             @Override
-            public String encode(CharSequence rawPassword) {
-                return DigestUtil.md5Hex(rawPassword.toString());
+            public String encode(CharSequence charSequence) {
+                return DigestUtil.md5Hex(charSequence.toString());
             }
 
             /**
              * 校验密码
-             * @param rawPassword       原始密码
-             * @param encodedPassword   加密密码
+             * @param charSequence 原始密码
+             * @param s 加密密码
              * @return
              */
             @Override
-            public boolean matches(CharSequence rawPassword, String encodedPassword) {
-                return DigestUtil.md5Hex(rawPassword.toString()).equals(encodedPassword);
+            public boolean matches(CharSequence charSequence, String s) {
+                return DigestUtil.md5Hex(charSequence.toString()).equals(s.toString());
             }
         };
     }
 
-    // 初始化认证管理对象
-    @Bean
+    //初始化认证管理对象
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
 
-    // 放行和认证规则
+    //放行和认证规则
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                // 放行的请求
+                //放行的请求
                 .antMatchers("/oauth/**", "/actuator/**").permitAll()
                 .and()
                 .authorizeRequests()
-                // 其他请求必须认证才能访问
                 .anyRequest().authenticated();
     }
-
 }
