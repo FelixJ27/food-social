690ff817078a7146bdfa9135dacde5409b5be019
felix <1045985453@qq.com>
2024-03-30 14:53:47 +0800

    压测代码（会产生超卖问题）

--- ms-seckill/src/main/java/com/imooc/seckill/controller/SeckillController.java
+++ ms-seckill/src/main/java/com/imooc/seckill/controller/SeckillController.java
@@ -4,6 +4,7 @@ import com.imooc.commons.model.domain.ResultInfo;
 import com.imooc.commons.model.pojo.SeckillVouchers;
 import com.imooc.commons.utils.ResultInfoUtil;
 import com.imooc.seckill.service.SeckillService;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,20 @@ public class SeckillController {
     private SeckillService seckillService;
     @Resource
     private HttpServletRequest request;
+
+    /**
+     * 秒杀下单
+     *
+     * @param voucherId
+     * @param access_token
+     * @return
+     */
+    @PostMapping("{voucherId}")
+    public ResultInfo<String> doSeckill(@PathVariable Integer voucherId, String access_token) {
+        ResultInfo resultInfo = seckillService.doSeckill(voucherId, access_token, request.getServletPath());
+        return resultInfo;
+    }
+
 
     /**
      * 新增秒杀活动
--- ms-seckill/src/main/java/com/imooc/seckill/service/SeckillService.java
+++ ms-seckill/src/main/java/com/imooc/seckill/service/SeckillService.java
@@ -1,13 +1,24 @@
 package com.imooc.seckill.service;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.IdUtil;
+import com.imooc.commons.constant.ApiConstant;
+import com.imooc.commons.model.domain.ResultInfo;
 import com.imooc.commons.model.pojo.SeckillVouchers;
+import com.imooc.commons.model.pojo.VoucherOrders;
+import com.imooc.commons.model.vo.SignInDinerInfo;
 import com.imooc.commons.utils.AssertUtil;
+import com.imooc.commons.utils.ResultInfoUtil;
 import com.imooc.seckill.mapper.SeckillVouchersMapper;
+import com.imooc.seckill.mapper.VoucherOrdersMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 import java.util.Date;
+import java.util.LinkedHashMap;
 
 /**
  * 秒杀业务逻辑层
@@ -16,7 +27,68 @@ import java.util.Date;
 public class SeckillService {
 
     @Resource
+    private VoucherOrdersMapper voucherOrdersMapper;
+    @Resource
     private SeckillVouchersMapper seckillVouchersMapper;
+    @Value("${service.name.ms-oauth-server}")
+    private String oauthServerName;
+    @Resource
+    private RestTemplate restTemplate;
+
+    /**
+     * 抢购代金券
+     *
+     * @param voucherId   代金券 ID
+     * @param accessToken 登录token
+     * @Para path 访问路径
+     */
+    public ResultInfo doSeckill(Integer voucherId, String accessToken, String path) {
+        // 基本参数校验
+        AssertUtil.isTrue(voucherId == null || voucherId < 0, "请选择需要抢购的代金券");
+        AssertUtil.isNotEmpty(accessToken, "请登录");
+        // 判断此代金券是否加入抢购
+        SeckillVouchers seckillVouchers = seckillVouchersMapper.selectVoucher(voucherId);
+        AssertUtil.isTrue(seckillVouchers == null, "该代金券并未有抢购活动");
+        // 判断是否有效
+        AssertUtil.isTrue(seckillVouchers.getIsValid() == 0, "该活动已结束");
+        // 判断是否开始、结束
+        Date now = new Date();
+        AssertUtil.isTrue(now.before(seckillVouchers.getStartTime()), "该抢购还未开始");
+        AssertUtil.isTrue(now.after(seckillVouchers.getEndTime()), "该抢购已结束");
+        // 判断是否卖完
+        AssertUtil.isTrue(seckillVouchers.getAmount() < 1, "该券已经卖完了");
+        // 获取登录用户信息
+        String url = oauthServerName + "user/me?access_token={accessToken}";
+        ResultInfo resultInfo = restTemplate.getForObject(url, ResultInfo.class, accessToken);
+        if (resultInfo.getCode() != ApiConstant.SUCCESS_CODE) {
+            resultInfo.setPath(path);
+            return resultInfo;
+        }
+        // 这里的data是一个LinkedHashMap，SignInDinerInfo
+        SignInDinerInfo dinerInfo = BeanUtil.fillBeanWithMap((LinkedHashMap) resultInfo.getData(),
+                new SignInDinerInfo(), false);
+        // 判断登录用户是否已抢到(一个用户针对这次活动只能买一次)
+        VoucherOrders order = voucherOrdersMapper.findDinerOrder(dinerInfo.getId(),
+                seckillVouchers.getId());
+        AssertUtil.isTrue(order != null, "该用户已抢到该代金券，无需再抢");
+        // 扣库存
+        int count = seckillVouchersMapper.stockDecrease(seckillVouchers.getId());
+        AssertUtil.isTrue(count == 0, "该券已经卖完了");
+        // 下单
+        VoucherOrders voucherOrders = new VoucherOrders();
+        voucherOrders.setFkDinerId(dinerInfo.getId());
+        voucherOrders.setFkSeckillId(seckillVouchers.getId());
+        voucherOrders.setFkVoucherId(seckillVouchers.getFkVoucherId());
+        String orderNo = IdUtil.getSnowflake(1, 1).nextIdStr();
+        voucherOrders.setOrderNo(orderNo);
+        voucherOrders.setOrderType(1);
+        voucherOrders.setStatus(0);
+        count = voucherOrdersMapper.save(voucherOrders);
+        AssertUtil.isTrue(count == 0, "用户抢购失败");
+
+        return ResultInfoUtil.buildSuccess(path, "抢购成功");
+    }
+
 
     /**
      * 添加需要抢购的代金券
